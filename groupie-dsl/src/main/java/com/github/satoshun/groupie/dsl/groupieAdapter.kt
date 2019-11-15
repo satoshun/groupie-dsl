6aa92342e6da2fa001e3b3054c743568a2f878cd
SatoShun <shun.sato1@gmail.com>
2019-11-15 00:10:42 +0000

    first

---
+++ groupie-dsl/src/main/java/com/github/satoshun/groupie/dsl/groupieAdapter.kt
@@ -0,0 +1,137 @@
+package com.github.satoshun.groupie.dsl
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import com.xwray.groupie.ExpandableGroup
+import com.xwray.groupie.ExpandableItem
+import com.xwray.groupie.Group
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import java.util.concurrent.atomic.AtomicLong
+
+fun groupieAdapter(block: BuilderGroupAdapter.() -> Unit): BuilderGroupAdapter =
+  BuilderGroupAdapter().apply {
+    block()
+    addAll()
+  }
+
+private val ID_COUNTER = AtomicLong(0)
+
+class BuilderGroupAdapter : GroupAdapter<GroupieViewHolder>() {
+  private var items: MutableList<Group> = mutableListOf()
+
+  fun update(block: BuilderGroupAdapter.() -> Unit): BuilderGroupAdapter =
+    BuilderGroupAdapter().apply {
+      block()
+      updateAll()
+    }
+
+  operator fun Group.unaryPlus() {
+    items.plusAssign(this)
+  }
+
+  fun item(
+    @LayoutRes layoutRes: Int,
+    data: Any? = null,
+    block: View.(Int) -> Unit
+  ) {
+    items.plusAssign(
+      BuilderItem(
+        data?.hashCode()?.toLong() ?: ID_COUNTER.decrementAndGet(),
+        layoutRes,
+        data,
+        block
+      )
+    )
+  }
+
+  fun expandable(
+    @LayoutRes layoutRes: Int,
+    block: View.(Int, ExpandableGroup) -> Unit,
+    expandedBlock: BuilderExpandableGroup.() -> Unit
+  ) {
+    items.plusAssign(
+      BuilderExpandableGroup(
+        ExpandableBuilderItem(
+          layoutRes,
+          block
+        )
+      ).apply(
+        expandedBlock
+      )
+    )
+  }
+
+  internal fun addAll() {
+    addAll(items)
+    items.clear()
+  }
+
+  private fun updateAll() {
+    update(items)
+    items.clear()
+  }
+}
+
+class BuilderExpandableGroup(group: Group) : ExpandableGroup(group) {
+  fun item(
+    @LayoutRes layoutRes: Int,
+    data: Any? = null,
+    block: View.(Int) -> Unit
+  ) {
+    add(
+      BuilderItem(
+        data?.hashCode()?.toLong() ?: ID_COUNTER.decrementAndGet(),
+        layoutRes,
+        data,
+        block
+      )
+    )
+  }
+
+  fun expandable(
+    @LayoutRes layoutRes: Int,
+    block: View.(Int, ExpandableGroup) -> Unit,
+    expandableBlock: BuilderExpandableGroup.() -> Unit
+  ) {
+    add(
+      BuilderExpandableGroup(
+        ExpandableBuilderItem(
+          layoutRes,
+          block
+        )
+      ).apply(expandableBlock))
+  }
+}
+
+data class BuilderItem(
+  private val _id: Long,
+  @LayoutRes private val layoutRes: Int,
+  private val any: Any?,
+  private val block: View.(Int) -> Unit
+) : Item<GroupieViewHolder>(_id) {
+  override fun getLayout(): Int = layoutRes
+
+  override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+    viewHolder.root.block(position)
+  }
+}
+
+class ExpandableBuilderItem(
+  @LayoutRes private val layoutRes: Int,
+  private val block: View.(Int, ExpandableGroup) -> Unit
+) : Item<GroupieViewHolder>(), ExpandableItem {
+
+  private var onToggleListener: ExpandableGroup? = null
+
+  override fun getLayout(): Int = layoutRes
+
+  override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+    viewHolder.root.block(position, onToggleListener!!)
+  }
+
+  override fun setExpandableGroup(onToggleListener: ExpandableGroup) {
+    this.onToggleListener = onToggleListener
+  }
+}
---
+++ sample/src/main/java/com/github/satoshun/example/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.github.satoshun.example
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.satoshun.example.databinding.MainActBinding
+import com.github.satoshun.example.databinding.MainItem1Binding
+import com.github.satoshun.example.databinding.MainItem2Binding
+import com.github.satoshun.groupie.dsl.groupieAdapter
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: MainActBinding
+
+  @SuppressLint("SetTextI18n")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = MainActBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.recycler.layoutManager = LinearLayoutManager(this)
+
+    binding.recycler.adapter = groupieAdapter {
+      item(R.layout.main_item1) {
+        val binding = MainItem1Binding.bind(this)
+        binding.title.text = "Main1"
+      }
+
+      item(R.layout.main_item2) {
+        val binding = MainItem2Binding.bind(this)
+        binding.title.text = "Main2"
+      }
+    }
+  }
+}
